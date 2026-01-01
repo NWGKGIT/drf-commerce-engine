e4f90c72b466b04966fae6afebee4e456c0251e6
Nahom Gebremichael <119551720+NWGKGIT@users.noreply.github.com>
2026-01-01 20:47:12 +0300

    fix(core): added urls

--- drf_commerce_engine/urls.py
+++ drf_commerce_engine/urls.py
@@ -1,22 +1,45 @@
-"""
-URL configuration for drf_commerce_engine project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from accounts.views import SecureAdminSetupView
+from rest_framework.routers import DefaultRouter
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularSwaggerView,
+    SpectacularRedocView,
+)
+from accounts import urls
+from django.http import HttpResponse
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
+    path("admin/", admin.site.urls),
+    # DRF Auth Endpoints
+    path("api/auth/", include("dj_rest_auth.urls")),
+    path("api/auth/registration/", include("dj_rest_auth.registration.urls")),
+    path(
+        "api/accounts/", include("accounts.urls")
+    ),  # account related shit like addresses.
+    path("api/", include("products.urls")),
+    path("auth/", include("allauth.urls")),
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path(
+        "api/schema/swagger-ui/",
+        SpectacularSwaggerView.as_view(url_name="schema"),
+        name="swagger-ui",
+    ),
+    path(
+        "api/schema/redoc/",
+        SpectacularRedocView.as_view(url_name="schema"),
+        name="redoc",
+    ),
+    path("api/cart/", include("cart.urls"), name="cart"),
+    path("api/auth/init-admin/", SecureAdminSetupView.as_view(), name="init-admin"),
+    path("api/orders/", include("orders.urls"), name="orders"),
+    path("api/reviews/", include("reviews.urls"), name="reviews"),
+    path("api/wishlist/", include("wishlist.urls"), name="wishlist"),
+    path("api/payments/", include("payments.urls")),
+    path(
+        "password-reset/confirm/<uidb64>/<token>/",
+        lambda r, uidb64, token: HttpResponse("Post the new password to /api/auth/password/reset/confirm/"),
+        name="password_reset_confirm",
+    ), #crude fix because we don't have a frontend
 ]
