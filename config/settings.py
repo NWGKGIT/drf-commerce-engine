2fc6aea9b3ad357a570f11e3d4e5487a97d91ab2
Nahom Gebremichael <119551720+NWGKGIT@users.noreply.github.com>
2026-01-28 23:34:38 +0300

    fix: login redirection after email verification

--- config/settings.py
+++ config/settings.py
@@ -147,8 +147,9 @@ ACCOUNT_SIGNUP_FIELDS = {
     "first_name": {"required": False},
     "last_name": {"required": False},
 }
-# LOGIN_URL = '/auth/login/'
-# LOGIN_REDIRECT_URL = '/api/'
+
+LOGIN_URL = '/auth/login/'
+LOGIN_REDIRECT_URL = '/api/'
 ACCOUNT_EMAIL_CONFIRMATION_AUTHENTICATED_REDIRECT_URL='/api/'
 
 # --- REST Framework ---
