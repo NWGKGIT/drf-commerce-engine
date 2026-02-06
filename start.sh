dfbb0f4b75501ce2290aa11d38f7f476b16ee109
Nahom Gebremichael <119551720+NWGKGIT@users.noreply.github.com>
2026-02-06 22:30:16 +0300

    disable celery

--- start.sh
+++ start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 set -o errexit
 
-celery -A config worker --beat -l info --concurrency=2 --pidfile= &
+# celery -A config worker --beat -l info --concurrency=2 --pidfile= &
 
 gunicorn config.wsgi:application
