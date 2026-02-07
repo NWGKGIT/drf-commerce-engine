2ef65c20d504611197bd0c63e0a872b1bc1750b5
Nahom Gebremichael <119551720+NWGKGIT@users.noreply.github.com>
2026-02-08 01:21:10 +0300

    temp view for flushing

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from django.core.management import call_command
+from django.http import HttpResponse
+# Temporary view for flushing database
 
-# Create your views here.
+def flush_database(request):
+    call_command('flush', interactive=False)
+    return HttpResponse("Database flushed successfully")
+
+# YOU SHOULD 100% REMOVE THIS VIEW BEFORE DEPLOYMENT
